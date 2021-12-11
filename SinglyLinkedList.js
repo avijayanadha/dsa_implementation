8a3200f536bb96073754d9d2db7073d24e9e9a60
avijayanadha <arunbabu.mvk@gmail.com>
2021-12-11 18:57:09 +0530

    Pop and shift

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -32,6 +32,42 @@ class SinglyLinkedList {
 
     pop() {
         /*
+        Remove the last element in the list
+        1 - > 2 -> 3 -> 4 -> 5
+        removes 5 in this example
+        
+        - if the list is empty return null
+        - if the list is of length 1 return head and set head and tail as null
+        - else iterate and find second to last element and returns its next node and point second to last element next as null also set tail as the second to last node
+        */
+        if (this.head === null) {
+            return null;
+        } else if (this.head === this.tail) {
+            let head = this.head;
+            this.head = null;
+            this.tail = null;
+            this.length--;
+
+            return head;
+        } else {
+            let node = this.head;
+            let previousNode;
+
+            while(node.next !== null) {
+                previousNode = node;
+                node = node.next;
+            }
+            
+            previousNode.next = null;
+            this.tail = previousNode;
+            this.length--;
+            return node;
+
+        }
+    }
+
+    shift() {
+        /*
         Removes the first element from the list and return it
         - if the list is empty return null
         - if the list has only one element return that and set head and tail as null
